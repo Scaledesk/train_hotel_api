39281c84ec72d9a9454631bacdcdc041161b34a3
Javed Ahamad <javed@scaledesk.com>
2015-12-05 19:43:17 +0530

    added function to filter hotels data and display them

--- assets/js/controllers/SearchHotellController.js
+++ assets/js/controllers/SearchHotellController.js
@@ -1,7 +1,6 @@
 angular.module('Traveller')
 // inject the Activation service into our controller
     .controller('SearchHotelController', function($http, $scope, Hotel,$routeParams,$filter, $location) {
-
         var hotelRequest = '<MMTHotelSearchRequest><POS><Requestor type="B2C" idContext="AFF" id="AFF322603" channel="B2Cweb"/> <Source iSOCurrency="INR"/> </POS> <ResultTransformer> ' +
             '<GuestRecommendationEnabled maxRecommendations="1">true</GuestRecommendationEnabled> ' +
             '<PriceBreakupEnabled>true</PriceBreakupEnabled> <CancellationPolicyRulesReq text="yes"/> ' +
@@ -18,11 +17,11 @@ angular.module('Traveller')
 
 
         var getCountryRequest = '<MMTStaticCountrySearchRequest Offset="0" Rows="100">' +
-                '<POS>' +
-                    '<Requestor type="AFF" idContext="AFF" id="AFF322603" channel="AFF"/>' +
-                    '<Source iSOCurrency="INR"/>' +
-                    '<Token>AFF322603</Token>' +
-                '</POS>' +
+            '<POS>' +
+            '<Requestor type="AFF" idContext="AFF" id="AFF322603" channel="AFF"/>' +
+            '<Source iSOCurrency="INR"/>' +
+            '<Token>AFF322603</Token>' +
+            '</POS>' +
             '<RequestCountryParams>' +
             '</RequestCountryParams>' +
             '</MMTStaticCountrySearchRequest>';
@@ -40,96 +39,77 @@ angular.module('Traveller')
             '</RequestCityParams>' +
             '<RequiredFields>' +
             'country, countryCode' +
-        '</RequiredFields>' +
-        '</MMTStaticCitySearchRequest>';
+            '</RequiredFields>' +
+            '</MMTStaticCitySearchRequest>';
 
-
-
-
-        $scope.searchHotels = function(){
+        $scope.hotelResult = [];
+        // function for search hotels
+        $scope.searchHotels = function () {
             Hotel.search(hotelRequest).then(
-                function(data){
-                    console.log(data);
+                function (data) {
+                    //console.log(data);
                     $scope.hotels = data;
+                    angular.forEach($scope.hotels.data.HotelSearchResults.Hotels, function (hotel, key) {
+                        var mVal = '';
 
+                        angular.forEach(hotel.PropertyInfo.RoomStays, function (rates, key) {
+
+                            var start_date = rates.startDate;
+                            var end_date = rates.endDate;
+                            angular.forEach(rates.RoomRates, function (rate, key) {
+                            angular.forEach(rate.Rate.RoomTariffs, function (r, key) {
+                                t = {
+                                    "room_number" : r.roomNumber,
+                                      "amount" : r.Tariffs[0].amount,
+                                    "group" : r.Tariffs[0].group,
+                                    "startDate" : start_date,
+                                    "endDate" : end_date
+                                };
+                            });
+                            });
+                        });
+                        angular.forEach(hotel.PropertyInfo.MediaList, function (m, key) {
+                            mVal = {
+                                "type": m.type,
+                                "src": m.src
+                            };
+                        });
+                        var temp = {
+                            "hotel_name": hotel.Name,
+                            "hotel_id" : hotel.id,
+                            "media": mVal,
+                            "lowest_rate": hotel.LowestRate.LowestRate.value,
+                            "total_recommendation": hotel.PropertyInfo.GuestRecommendations.totalRecommendations,
+                            "total_rating_value": hotel.PropertyInfo.GuestRecommendations.OverallRecommendation.Rating.value,
+                            "rate" : t
+                        };
+                        $scope.hotelResult.push(temp);
+                    });
                 },
-                function(data){
-                   // $scope.hotels = data;
+                function (data) {
+                    // $scope.hotels = data;
                 });
         };
 
-        $scope.getCountry =function(){
+        // function for getting all countries
+        $scope.getCountry = function () {
             Hotel.getCountry(getCountryRequest).then(
-                function(data){
+                function (data) {
                     $scope.countries = data;
-
                 },
-                function(data){
+                function (data) {
                     // $scope.countries = data;
                 });
         };
 
-        $scope.getCity =function(){
+        // function for getting city
+        $scope.getCity = function () {
             Hotel.getCity(getCityRequest).then(
-                function(data){
+                function (data) {
                     $scope.cities = data;
-
                 },
-                function(data){
+                function (data) {
                     // $scope.cities = data;
                 });
         };
-
-
-        angular.forEach($scope.hotels.data.HotelSearchResults.Hotels, function(hotel, key) {
-
-            //var h_name = hotel.name;
-           // angular.forEach(hotal, function(h, key) {
-
-
-            angular.forEach(hotel.hotel.PropertyInfo.MediaList, function(m, key){
-                $scope.media = {
-                    "type": m.type,
-                    "src": m.src
-                }
-            });
-
-            var tmp ={
-                "hotel_name" : hotel.name,
-                "total_recommendation" : hotel.PropertyInfo.GuestRecommendations.totalRecommendations,
-                "total_rating_value" : hotel.PropertyInfo.GuestRecommendations.OverallRecommendation.Rating.value,
-
-
-
-            }
-
-        });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     });
